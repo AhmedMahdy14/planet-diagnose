d536ed19a92a996c1de7e09f3eca75d09536f168
AhmedAE14 <150889787+AhmedAE14@users.noreply.github.com>
2024-06-20 13:54:39 +0300

    updates

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 from inference_sdk import InferenceHTTPClient
-from flask_socketio import SocketIO
-from pymongo import MongoClient
-import threading
+from flask_sqlalchemy import SQLAlchemy
 
 cloudinary.config(cloud_name="ddgeg9myx", api_key="911351556278827", api_secret="i9GCIpqx7AkzfLtUcUsFVYg652o")
 
@@ -20,44 +18,70 @@ MODEL_ID = "plant-disease-kkt3g/1"
 
 app = Flask(__name__)
 
-socketio = SocketIO(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgres://uatngc8vpchqgn:ped841106285a3fde3b5b56635ebd8d23ef777ed2efa4583995eedb32feaf8acb@c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dds4v25hrjtbs5')
+db = SQLAlchemy(app)
+
+
+class SensorData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    temperature = db.Column(db.Float, nullable=False)
+    humidity = db.Column(db.Float, nullable=False)
+
+
+@app.route('/add', methods=['POST'])
+def add_data():
+    data = request.json
+    new_entry = SensorData(temperature=data['temperature'], humidity=data['humidity'])
+    db.session.add(new_entry)
+    db.session.commit()
+    return jsonify({"message": "Data added"}), 201
+
+
+@app.route('/data', methods=['GET'])
+def get_data():
+    results = SensorData.query.all()
+    data = [{"temperature": entry.temperature, "humidity": entry.humidity} for entry in results]
+    return jsonify(data)
+
+
+# socketio = SocketIO(app)
 
 # MongoDB connection
-client = MongoClient(
-    'mongodb+srv://ahmedmahdy1420:p1FiayTc5IxFt5De@cluster0.18q1cq3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true')
-db = client.sensor_data
-collection = db.readings
+# client = MongoClient(
+#     'mongodb+srv://ahmedmahdy1420:p1FiayTc5IxFt5De@cluster0.18q1cq3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+# db = client.sensor_data
+# collection = db.readings
 
 
 # Fetch data from MongoDB
-def fetch_data():
-    cursor = collection.find().sort('timestamp', -1).limit(10)
-    return list(cursor)
+# def fetch_data():
+#     cursor = collection.find().sort('timestamp', -1).limit(10)
+#     return list(cursor)
 
 
 # Monitor MongoDB for changes
-def monitor_changes():
-    with collection.watch() as stream:
-        for change in stream:
-            socketio.emit('new_data', fetch_data())
+# def monitor_changes():
+#     with collection.watch() as stream:
+#         for change in stream:
+#             socketio.emit('new_data', fetch_data())
 
 
 # Run the monitor_changes function in a separate thread
-thread = threading.Thread(target=monitor_changes)
-thread.start()
-
+# thread = threading.Thread(target=monitor_changes)
+# thread.start()
+#
 
 @app.route('/')
 def index():
-    data = fetch_data()
-    print(data)
-    return render_template('index.html', data=data)
+    # data = fetch_data()
+    # print(data)
+    return render_template('index.html')
 
 
-@socketio.on('connect')
-def handle_connect():
-    socketio.emit('new_data', fetch_data())
-
+# @socketio.on('connect')
+# def handle_connect():
+#     socketio.emit('new_data', fetch_data())
+#
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -145,3 +169,40 @@ def perform_inference(file):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+# Host
+# c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com
+# Database
+# dds4v25hrjtbs5
+# User
+# uatngc8vpchqgn
+# Port
+# 5432
+# Password
+# ped841106285a3fde3b5b56635ebd8d23ef777ed2efa4583995eedb32feaf8acb
+# URI
+# postgres://uatngc8vpchqgn:ped841106285a3fde3b5b56635ebd8d23ef777ed2efa4583995eedb32feaf8acb@c5p86clmevrg5s.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dds4v25hrjtbs5
+# Heroku CLI
+# heroku pg:psql postgresql-colorful-94934 --app flask-app-ams
+
+# import requests
+#
+# def send_data_to_heroku(temperature, humidity):
+#     url = "https://your-app-name.herokuapp.com/add"
+#     data = {
+#         "temperature": temperature,
+#         "humidity": humidity
+#     }
+#     response = requests.post(url, json=data)
+#     print(response.json())
+#
+# # Example usage:
+# temperature = 22.5
+# humidity = 60
+# send_data_to_heroku(temperature, humidity)
+
+
+# heroku run python
+# >>> from app import db
+# >>> db.create_all()
+# >>> exit()
